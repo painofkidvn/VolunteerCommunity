c6c6a10c61c131dd0048ce18227103b041ab75c7
painofkidvn <painofkidvn@gmail.com>
2017-02-23 14:56:56 +0700

    Update Webservice for Register and Login

---
+++ app/src/main/java/com/hat_dtu/volunteercommunity/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.hat_dtu.volunteercommunity.app;
+
+/**
+ * Created by paino on 2/22/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "https://painofkidvn.000webhostapp.com/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "https://painofkidvn.000webhostapp.com/android_login_api/register.php";
+}
